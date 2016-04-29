ad47f5f5463bac07a57d24614d453aed55f10908
georgestrajan <george.strajan@gmail.com>
2016-04-29 10:57:32 +0700

    Now handles movies

--- Process.py
+++ Process.py
@@ -1,4 +1,7 @@
+# This script iterates through all of the files in a folder and prints the EXIF DateTimeOriginal tag
 import os
+import time
+import datetime
 import sys
 import exifread
 
@@ -8,18 +11,26 @@ src = os.path.abspath(src)
 # this command will count the number of pictures and videos in the folder
 # find /home/george/Pictures/Test -regex '.*\.\(jpg\|JPG\|mp4\|MP4\)' | tee >(wc -l)
 
-count = 0
+numberOfPictures = 0
+numberOfMovies = 0
+
 for root, subdirs, files in os.walk(src):
     path = root.split('/')
     for file in files:
         fullPath = os.path.join(root, file)
-        f = open(fullPath, 'rb')
         
-        tags = exifread.process_file(f)
-        for tag in tags.keys():
-            if tag in ('EXIF DateTimeOriginal'):
-                print "File: %s, Date %s" % (fullPath, tags[tag])
+        if file.lower().find(".jpg") > 0:
+            f = open(fullPath, 'rb')        
+            tags = exifread.process_file(f)
+            for tag in tags.keys():
+                if tag in ('EXIF DateTimeOriginal'):
+                    print "File: %s, Date %s" % (fullPath, tags[tag])
+                    numberOfPictures = numberOfPictures + 1
         
-        count = count + 1
-
-print "Number of files:", count
+        if file.lower().find(".mp4") > 0 or file.lower().find(".avi") > 0:
+            print "File: %s, Date %s" % (fullPath, 
+            datetime.datetime.strptime(time.ctime(os.path.getctime(fullPath)), "%a %b %d %H:%M:%S %Y"))
+            numberOfMovies = numberOfMovies + 1
+            
+print "Number of pictures:", numberOfPictures
+print "Number of movies:", numberOfMovies
